d235bf5aa17d1d0e109f57ff21130bef970a41ba
PrinceDevadoss <prince.devadoss@freshworks.com>
2020-06-27 11:35:40 +0530

    layout changes

--- src/components/about.js
+++ src/components/about.js
@@ -1,56 +1,50 @@
 import React from 'react';
 class About extends React.Component {
-    render() {
-        return (
-        <>
+  render() {
+    return (
+      <>
         <section id="about" class="about">
-      
-      
-        <div class="img"></div>
-          <div class="col-xl-7 pl-0 pl-lg-5 pr-lg-1 d-flex align-items-stretch"></div>
-            <div class="content">
-            
-              <h3><center>About Us</center></h3>
-              <p>
+          <div class="about-img">
+            <img src="/assets/img/recycle.gif"/>
+          </div>
+          <div class="about-content">
+            <h3>About Us</h3>
+            <p>
               At Earth Recycler, the concept of solid waste management is redefined through professional and systematic process incorporating awareness program on source segregation, collection, transportation, recycling and scientific disposal of waste to ensure clean and hygienic surroundings.
               For us, waste is considered to be a resource that can fetch a higher value beyond their first use phase by applying recycling process that are more robust and environmental friendly.
               From low grade polythene bag, used tissue paper to paper cups we have solutions to recycle them all.
-              
-              </p>
+            </p>
+            <div class="row">
+              <div class="row-content">
+                <i class="bx bx-receipt"></i>
+                <h4>What We Do</h4>
+                <p>Earth Recycler work towards ‘Zero Landfill’.All the scrap that is given to us is recycled, diverted and becomes a useful product.</p>
               </div>
-              <div class="row">
-                
-                  <i class="bx bx-receipt"></i>
-                  <h4>What We Do</h4>
-                  <p>Earth Recycler work towards ‘Zero Landfill’.All the scrap that is given to us is recycled, diverted and becomes a useful product.</p>
-                
-                <div class="row2">
-                  <i class="bx bx-cube-alt"></i>
-                  <h4>Vision</h4>
-                  <p>Engaging us will make it a win-win combination not only for both you and us, but also for the environment</p>
-                </div>
-                </div>
-                <div class="col1">
-                  <i class="bx bx-images"></i>
-                  <h4>Mission</h4>
-                  <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
-                </div>
-                <div class="col2">
-                  <i class="bx bx-shield"></i>
-                  <h4>Beatae veritatis</h4>
-                  <p>Expedita veritatis consequuntur nihil tempore laudantium vitae denat pacta</p>
-                  </div>
-                
-              
-            
-         
-    </section>
-   
+              <div class="row-content">
+                <i class="bx bx-cube-alt"></i>
+                <h4>Vision</h4>
+                <p>Engaging us will make it a win-win combination not only for both you and us, but also for the environment</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="row-content">
+                <i class="bx bx-images"></i>
+                <h4>Mission</h4>
+                <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
+              </div>
+              <div class="row-content">
+                <i class="bx bx-shield"></i>
+                <h4>Beatae veritatis</h4>
+                <p>Expedita veritatis consequuntur nihil tempore laudantium vitae denat pacta</p>
+              </div>
+            </div>
+          </div>
+        </section>
 
-        </>
+
+      </>
     );
-    }
   }
-  export default About;        
+}
+export default About;
 
-         
